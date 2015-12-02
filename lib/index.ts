1772504bfc65e416b4e2a65fc92cee54107d3298
vvakame <vvakame+dev@gmail.com>
2015-12-02 21:56:20 +0900

    refactor index.ts. add author modification

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,125 @@
 import * as github from "./github";
 import * as header from "definition-header";
 
+export interface ReviewResult {
+    parent: github.PRInfo;
+    file: github.PullRequestFile;
+    baseHeader?: header.Result;
+    authorAccounts: string[];
+    unknownAuthors: header.model.Author[];
+    message?: string;
+}
+
+function processAdded(reviewResult: ReviewResult): ReviewResult {
+    "use strict";
+
+    let comment = "";
+    function log(text: string) {
+        comment += text + "\n";
+    }
+
+    let info = reviewResult.parent;
+    let file = reviewResult.file;
+
+    let packageName = file.filename.substr(0, file.filename.indexOf("/"));
+    let testFileNames = [file.filename.substr(0, file.filename.length - 5) + "-tests.ts"];
+    testFileNames[1] = testFileNames[0] + "x";
+    let testFileExists = info.files.filter(f => testFileNames.indexOf(f.filename) !== -1).length !== 0;
+
+    log(`Checklist`);
+    log(``);
+    log(`* [ ] is correct [naming convention](http://definitelytyped.org/guides/contributing.html#naming-the-file)?`);
+    log(`  * https://www.npmjs.com/package/${packageName}`);
+    log(`  * http://bower.io/search/?q=${packageName}`);
+    log(`  * others?`);
+    log(`* [${testFileExists ? "X" : " "}] has a [test file](http://definitelytyped.org/guides/contributing.html#tests)? (${testFileNames.join(" or ")})`);
+    log(`* [ ] pass the Travis CI test?`);
+
+    reviewResult.message = comment;
+
+    return reviewResult;
+}
+
+function convertAuthorToAccount(author: header.model.Author): string[] {
+    "use strict";
+
+    switch (author.url) {
+        case "https://asana.com":
+            return ["@pspeter3", "@vsiao"];
+        case "http://phyzkit.net/":
+            return ["@kontan"];
+        case "http://ianobermiller.com":
+            return ["@ianobermiller"];
+        default:
+            return null;
+    }
+    return null;
+}
+
+function processModified(reviewResult: ReviewResult): ReviewResult {
+    "use strict";
+
+    let comment = "";
+    function log(text: string) {
+        comment += text + "\n";
+    }
+
+    let info = reviewResult.parent;
+    let file = reviewResult.file;
+
+    let content = info.baseContents[file.filename];
+    let headerInfo = header.parse(content);
+    if (!headerInfo.success) {
+        reviewResult.message = "can't parse definition header...";
+        return reviewResult;
+    }
+    reviewResult.baseHeader = headerInfo;
+    headerInfo.value.authors.forEach(author => {
+        let accountNames = convertAuthorToAccount(author);
+        if (accountNames) {
+            reviewResult.authorAccounts = reviewResult.authorAccounts.concat(accountNames);
+            return;
+        }
+
+        let regexp = /https?:\/\/github.com\/(.*)\/?/;
+        let reArray: string[] = regexp.exec(author.url) || [];
+        let accountName = reArray[1];
+        if (accountName) {
+            reviewResult.authorAccounts.push(`@${accountName}`);
+        } else {
+            reviewResult.unknownAuthors.push(author);
+        }
+    });
+
+    let accountNames: string[] = [].concat(reviewResult.authorAccounts);
+
+    reviewResult.unknownAuthors.forEach(author => {
+        accountNames.push(`${author.name} (account can't be detected)`);
+    });
+
+    if (accountNames.length !== 0) {
+        log(`to author${accountNames.length === 1 ? "" : "s"} (${accountNames.join(" ")}). Could you review this PR?`);
+        log(":+1: or :-1:?");
+    }
+
+    log(``);
+    log(`Checklist`);
+    log(``);
+    log(`* [ ] pass the Travis CI test?`);
+
+    reviewResult.message = comment;
+
+    return reviewResult;
+}
+
 export function generateComment(pr: github.PRInfoRequest): Promise<string[]> {
+    "use strict";
+
+    return constructReviewResult(pr)
+        .then(ts => ts.map(result => [`*${result.file.filename}*`, "", result.message].join("\n")));
+}
+
+export function constructReviewResult(pr: github.PRInfoRequest): Promise<ReviewResult[]> {
     "use strict";
 
     return github
@@ -10,59 +128,22 @@ export function generateComment(pr: github.PRInfoRequest): Promise<string[]> {
             return info.files
                 .filter(file => /\.d\.ts(x)?$/.test(file.filename))
                 .map((file, idx, files) => {
-                    let comment = "";
-                    function log(text: string) {
-                        comment += text + "\n";
-                    }
-
-                    log(`*${file.filename}*`);
-                    log("");
+                    let reviewResult: ReviewResult = {
+                        parent: info,
+                        file: file,
+                        authorAccounts: [],
+                        unknownAuthors: [],
+                    };
 
                     if (file.status === "modified") {
-                        let content = info.baseContents[file.filename];
-                        let headerInfo = header.parse(content);
-                        if (!headerInfo.success) {
-                            log("!!! TODO !!!");
-                            return;
-                        }
-                        let accountNames = headerInfo.value.authors.map(author => {
-                            let regexp = /https?:\/\/github.com\/(.*)\/?/;
-                            let reArray: string[] = regexp.exec(author.url) || [];
-                            let accountName = reArray[1];
-                            if (accountName) {
-                                return `@${accountName}`;
-                            } else {
-                                return `${author.name} (account can't be detected)`;
-                            }
-                        });
-
-                        if (accountNames.length !== 0) {
-                            log(`to author${accountNames.length === 1 ? "" : "s"} (${accountNames.join(" ") }). Could you review this PR?`);
-                            log(":+1: or :-1:?");
-                        }
-
-                        log(``);
-                        log(`Checklist`);
-                        log(``);
-                        log(`* [ ] pass the Travis CI test?`);
-
+                        return processModified(reviewResult);
                     } else if (file.status === "added") {
-                        let packageName = file.filename.substr(0, file.filename.indexOf("/"));
-                        let testFileNames = [file.filename.substr(0, file.filename.length - 5) + "-tests.ts"];
-                        testFileNames[1] = testFileNames[0] + "x";
-                        let testFileExists = info.files.filter(f => testFileNames.indexOf(f.filename) !== -1).length !== 0;
-
-                        log(`Checklist`);
-                        log(``);
-                        log(`* [ ] is correct [naming convention](http://definitelytyped.org/guides/contributing.html#naming-the-file)?`);
-                        log(`  * https://www.npmjs.com/package/${packageName}`);
-                        log(`  * http://bower.io/search/?q=${packageName}`);
-                        log(`  * others?`);
-                        log(`* [${testFileExists ? "X" : " "}] has a [test file](http://definitelytyped.org/guides/contributing.html#tests)? (${testFileNames.join(" or ") })`);
-                        log(`* [ ] pass the Travis CI test?`);
+                        return processAdded(reviewResult);
                     }
 
-                    return comment;
+                    reviewResult.message = `unknown status: ${file.status}`;
+
+                    return reviewResult;
                 });
         });
 }
