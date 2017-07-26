c2296a2a61dc2630ef3a601cdb9998777446b303
Leonard Thieu <leonard.thieu@gmail.com>
2017-07-26 17:29:14 -0400

    Refactor getPRInfo() to use async/await.

--- lib/github.ts
+++ lib/github.ts
@@ -4,7 +4,7 @@ import Client = require("github");
 
 export interface PRInfo {
     pr: GithubAPIv3.PullRequest;
-    files: GithubAPIv3.PullRequest.File[] | null;
+    files: GithubAPIv3.PullRequest.File[];
     contents: { [path: string]: string; };
     baseContents: { [path: string]: string; };
 }
@@ -15,7 +15,7 @@ export interface PRInfoRequest {
     number: number;
 }
 
-export function getPRInfo(req: PRInfoRequest): Promise<PRInfo> {
+export async function getPRInfo(req: PRInfoRequest): Promise<PRInfo> {
     let github = new Client({
         // debug: true
     });
@@ -25,83 +25,64 @@ export function getPRInfo(req: PRInfoRequest): Promise<PRInfo> {
         secret: "7524eed1afd84b09f08f1439e7e08860add37c09",
     });
 
-    return new Promise<PRInfo>((resolve, reject) => {
-        github.pullRequests.get({
-            owner: req.owner || "DefinitelyTyped",
-            repo: req.repo || "DefinitelyTyped",
-            number: req.number,
-        }, (err: any, res: GithubAPIv3.Response<GithubAPIv3.PullRequest>) => {
-            if (err) {
-                reject(err);
+    let owner = req.owner || "DefinitelyTyped";
+    let repo = req.repo || "DefinitelyTyped";
+
+    // Get the pull request
+    let pullRequestResponse = await github.pullRequests.get({
+        owner: owner,
+        repo: repo,
+        number: req.number,
+    });
+    let info: PRInfo = {
+        pr: pullRequestResponse.data,
+        files: [],
+        contents: {},
+        baseContents: {},
+    };
+
+    // Get the pull request's files
+    let pullRequestFilesResponse = await github.pullRequests.getFiles({
+        owner: owner,
+        repo: repo,
+        number: req.number,
+    });
+    info.files = pullRequestFilesResponse.data;
+
+    // Get the pull request's files' contents
+    let blobRequests = info.files.map(file => {
+        return github.gitdata.getBlob({
+            owner: owner,
+            repo: repo,
+            sha: file.sha,
+        }).then((res: GithubAPIv3.Response<GithubAPIv3.GitData.Blob>) => {
+            if (res.data.encoding === "utf-8") {
+                info.contents[file.filename] = res.data.content;
             } else {
-                resolve({
-                    pr: res.data,
-                    files: null,
-                    contents: {},
-                    baseContents: {},
-                });
+                let b = new Buffer(res.data.content, "base64");
+                info.contents[file.filename] = b.toString();
             }
         });
-    })
-        .then(info => {
-            return new Promise<PRInfo>((resolve, reject) => {
-                github.pullRequests.getFiles({
-                    owner: req.owner || "DefinitelyTyped",
-                    repo: req.repo || "DefinitelyTyped",
-                    number: req.number,
-                }, (err: any, res: GithubAPIv3.Response<GithubAPIv3.PullRequest.File[]>) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        info.files = res.data;
-                        resolve(info);
-                    }
-                });
-            });
-        }).then(info => {
-            let promises = info.files!.map(file => {
-                return new Promise<PRInfo>((resolve, reject) => {
-                    github.gitdata.getBlob({
-                        owner: req.owner || "DefinitelyTyped",
-                        repo: req.repo || "DefinitelyTyped",
-                        sha: file.sha,
-                    }, (err: any, res: GithubAPIv3.Response<GithubAPIv3.GitData.Blob>) => {
-                        if (err) {
-                            reject(err);
-                        } else if (res.data.encoding === "utf-8") {
-                            info.contents[file.filename] = res.data.content;
-                            resolve(info);
-                        } else {
-                            let b = new Buffer(res.data.content, "base64");
-                            info.contents[file.filename] = b.toString();
-                            resolve(info);
-                        }
-                    });
-                });
-            });
-            return Promise.all(promises).then(() => info);
-        }).then(info => {
-            let promises = info.files!.filter(file => file.status === "modified").map(file => {
-                return new Promise<PRInfo>((resolve, reject) => {
-                    github.repos.getContent({
-                        owner: "DefinitelyTyped",
-                        repo: "DefinitelyTyped",
-                        path: file.filename,
-                        ref: info.pr.base.ref,
-                    }, (err: any, res: GithubAPIv3.Response<GithubAPIv3.Repository.FileContents>) => {
-                        if (err) {
-                            reject(err);
-                        } else if (res.data.encoding === "utf-8") {
-                            info.baseContents[file.filename] = res.data.content;
-                            resolve(info);
-                        } else {
-                            let b = new Buffer(res.data.content, "base64");
-                            info.baseContents[file.filename] = b.toString();
-                            resolve(info);
-                        }
-                    });
-                });
-            });
-            return Promise.all(promises).then(() => info);
+    });
+    await Promise.all(blobRequests);
+
+    // Get the pull request's files' base contents
+    let fileContentsRequests = info.files.filter(file => file.status === "modified").map(file => {
+        return github.repos.getContent({
+            owner: owner,
+            repo: repo,
+            path: file.filename,
+            ref: info.pr.base.ref,
+        }).then((res: GithubAPIv3.Response<GithubAPIv3.Repository.FileContents>) => {
+            if (res.data.encoding === "utf-8") {
+                info.baseContents[file.filename] = res.data.content;
+            } else {
+                let b = new Buffer(res.data.content, "base64");
+                info.baseContents[file.filename] = b.toString();
+            }
         });
+    });
+    await Promise.all(fileContentsRequests);
+
+    return info;
 }
